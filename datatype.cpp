a3e13eb24711561b2c083702b996e9b4cd5d520f
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-05-03 13:07:52 +0530

    Create datatype.cpp

---
+++ datatype.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    printf("%.3f\n",d);
+    printf("%.9f\n",e);
+    return 0;
+    
+}
