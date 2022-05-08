792c10cfc651a22d318e02681d688720d25c3775
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-05-08 16:46:28 +0530

    Update PALINDROME.cpp

--- assignment-4/PALINDROME.cpp
+++ assignment-4/PALINDROME.cpp
@@ -1,32 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std;
-bool isPalindrome(string s)
-{
-            
-    int start = 0;
-    int end = s.length() - 1;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+            string s = to_string(x);
+            int start = 0;
+            int end = s.length() - 1;
 
-    while(start < end)
-    {
-        if(s[start++] != s[end--])
-        {
-            return false;
+            while(start < end)
+            {
+                if(s[start++] != s[end--])
+                    return false;
+            }
+
+            return true;
         }
-    }
-    return true;
-}
-int main()
-{
-    string s;
-    cin >> s;
-    if(isPalindrome(s)==true)
-    {
-        cout << "true";
-        
-    }
-    else
-    {
-        cout << "false";
-    }
-    return 0;
-}
+};
