cc24dae7e56bad4b5f5f38731ebf9553460ba062
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-05-09 10:17:31 +0530

    Create mergesortedarray.cpp

---
+++ code/mergesortedarray.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ vector<int>C;
+vector<int>mergesortedarray(vector<int>A,vector<int>B){
+    int i=0,j=0;
+   
+    while(i<A.size() && j<B.size()){
+        if(A[i]<B[j]){
+            C.push_back(A[i]);
+            i++;
+        }
+        else{
+         C.push_back(B[j]);  
+            j++;
+        }  
+    }
+      while(i<A.size()){
+          C.push_back(A[i]);
+          i++;
+      }
+        while(j<B.size()){
+            C.push_back(B[j]);
+            j++;
+           
+        }
+    return C;
+}
+int main(){
+  vector<int>A={1,2,5,6};
+    vector<int>B={1,3,4,7};
+    vector<int>C=mergesortedarray(A,B);
+    for(int a:C){
+        cout<<a<<" ";
+    }
+    return 0;
+   
+}
