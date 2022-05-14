5b2865000733dbf320fb4acbc50d0f434661b745
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-05-14 20:14:27 +0530

    Update birthdaycandles.cpp

--- assignment-1/birthdaycandles.cpp
+++ assignment-1/birthdaycandles.cpp
@@ -1,19 +1,30 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
+#include<vector>
 using namespace std;
-
 int main(){
-    int n,a[100000],maxm=0,j=0; 
-    cin>>n; 
-    for(int i=0;i<n;i++){ 
-    cin>>a[i];
+    int n;
+    cin >> n;
+    int count=0;
+    int arr[n];
+    int max= INT_MIN;
+    
+    for (int i=0; i < n ; i++){
+        cin >> arr[i];
     }
-    for(int i=0;i<n;i++){
-        maxm=max(maxm,a[i]);
-    } 
-    for(int i=0;i<n;i++){
-       if(a[i]==maxm){ 
-          j++; 
+    for (int i=0; i< n ; i++){
+        if (arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    for (int i=0; i< n ; i++){
+        if (arr[i]==max){
+            
+            count++;
         } 
-     }
-    cout<<j;
+        
+    }
+    cout << count << endl;
+    
+    return 0;
 }
