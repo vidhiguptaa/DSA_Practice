200140a1bd4510d2920c2ae351af656af0b50843
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-06-12 18:43:35 +0530

    Update countprime.cpp

--- assignment-3/countprime.cpp
+++ assignment-3/countprime.cpp
@@ -1,39 +1,33 @@
-#include <bits/stdc++.h>
-using namespace std;
-bool isprime(int n)
-{
-   if (n <= 1)
-   {
-      return 0;
-   }
-   for (int i = 2; i <= n/2; i++)
-   {
-      if (n % i == 0)
-      { 
-          return 0;
-      }
-   }
-   return 1;
-}
+class Solution {
+public:
+    bool isprime(int n)
+    {
+        if (n <= 1)
+        {
+            return 0;
+        }
+        for (int i = 2; i<=sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return 0;
+            }
+            
+        }
+        return 1;
+       
+    }
+    int countPrimes(int b)
+    {
+        int count=0;
+        for(int i=0;i<b;i++)
+        {
+            if(isprime(i)==1)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 
-int countprime(int b)
-{
-   int count=0;
-   for(int i=0;i<=b;i++)
-   {
-      if(isprime(i)==1)
-      {
-         count++;
-      }
-   }
-   return count;
-}
-int main()
-{
-   
-   int end;
-   cin >> end;
-   
-   cout <<countprime(end);
-   return 0;
-}
+};
