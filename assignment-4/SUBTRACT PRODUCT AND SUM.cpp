85650e4c41c35226c5fd4ac3776af024850e87ba
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-06-12 09:55:44 +0530

    Update SUBTRACT PRODUCT AND SUM.cpp

--- assignment-4/SUBTRACT PRODUCT AND SUM.cpp
+++ assignment-4/SUBTRACT PRODUCT AND SUM.cpp
@@ -1,22 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
-   
-int p=1,s=0;
-int subtractProductAndSum(int n)
-{
-        
-    cin >> n;
-    while(n>0)
+class Solution {
+public:
+    
+    int subtractProductAndSum(long n)
     {
-        p = p*(n%10);
-        s =s + (n%10);
-        n=n/10;
-    }
-    return p-s;
-}
-int main()
-{
-    int n;
-    cout << subtractProductAndSum(n);
-    return 0;
-}
+        cin >> n;
+        int p=1,s=0;
+        
+        while(n>0)
+        {
+            p = p * (n%10);
+            s = s + (n%10);
+            n=n/10;
+        }
+        return p-s;
+    };
+};
