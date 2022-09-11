ab5760cd3288761137218bed7b5d3e4aee989489
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-09-11 14:54:25 +0530

    Create Kth Distinct String in an Array.cpp

---
+++ Assignment-28/Kth Distinct String in an Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        string X="";
+        unordered_map<string, int>s;
+		for(int i=0; i<arr.size(); i++){
+            s[arr[i]]++;
+        }
+        for(int i=0; i<arr.size(); i++){
+			if(s[arr[i]]==1){
+                k--;
+                if(k==0) X =arr[i];
+            }
+            
+        }
+    return X;
+    }
+};
+
+
+https://leetcode.com/submissions/detail/796996577/
