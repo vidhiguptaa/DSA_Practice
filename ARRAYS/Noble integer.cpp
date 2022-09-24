4bac20fe1a368f4a5d9c590d8c999ad2162c7bdf
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-09-24 20:07:40 +0530

    Create Noble integer.cpp
    https://www.interviewbit.com/problems/noble-integer/

---
+++ ARRAYS/Noble integer.cpp
@@ -0,0 +1,26 @@
+int Solution::solve(vector<int> &A) 
+{
+    int n=A.size();
+    sort(A.begin(),A.end());
+    if(A[n-1]==0)
+    {
+        return 1;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]==A[i+1])
+        {
+            continue;
+        }
+        else
+        {
+            if(A[i]==n-i-1)
+            {
+                return 1;
+                
+            }
+        }
+    
+    }
+    return -1;
+}
