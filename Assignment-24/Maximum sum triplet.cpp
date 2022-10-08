b92dfc576c1cd70b7f968bd86e7b2861d1292b2b
vidhiguptaa <104747467+vidhiguptaa@users.noreply.github.com>
2022-10-08 14:24:16 +0530

    Create Maximum sum triplet.cpp

---
+++ Assignment-24/Maximum sum triplet.cpp
@@ -0,0 +1,22 @@
+int Solution::solve(vector<int> &arr) 
+{
+    int n= arr.size();
+    int ans=0;
+    for(int i=1;i<n-1;i++)
+    {
+        int max1 = 0, max2 = 0;
+        for (int j = 0; j < i; ++j)
+            if (arr[j] < arr[i])
+                max1 = max(max1, arr[j]);
+        for (int j = i + 1; j < n; ++j)
+            if (arr[j] > arr[i])
+                max2 = max(max2, arr[j]);
+        if(max1 && max2)
+             ans=max(ans,max1+arr[i]+max2);
+    }
+ 
+    return ans;
+        
+        
+    
+}
